674a0d5023e25881c7fe83d4018ae4100b26bc65
Kevin McWhirter <klmcwhirter@gmail.com>
2023-11-05 11:27:23 -0800

    Add TLS support, clean up compose file, add icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [
+    basicSsl(),
+
     /* 
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
@@ -14,6 +17,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    https: true,
   },
   build: {
     target: 'esnext',
